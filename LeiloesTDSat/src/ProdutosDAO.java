f452b233caeae350eeb97a18e94f0155348d63d1
Bleickh <felipebleichu@gmail.com>
2026-03-19 21:44:53 -0300

    Commit #4: listagem de produtos funcionando

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -30,10 +31,35 @@ public class ProdutosDAO {
         
     }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
+    String sql = "SELECT * FROM produtos";
+
+    try {
+        conn = new conectaDAO().connectDB();
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        listagem.clear();
+
+        while (resultset.next()) {
+
+            ProdutosDTO obj = new ProdutosDTO();
+
+            obj.setId(resultset.getInt("id"));
+            obj.setNome(resultset.getString("nome"));
+            obj.setValor(resultset.getInt("valor"));
+            obj.setStatus(resultset.getString("status"));
+
+            listagem.add(obj);
+        }
+
+        } catch (SQLException e) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar: " + e.getMessage());
     }
+
+    return listagem;
+}
     
     
     
